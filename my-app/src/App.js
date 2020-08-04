4709f54a4202b54a6d8d87d507853affc99cb6d3
mauro Leos <mauroleos94@gmail.com>
2020-08-04 08:39:56 -0500

    Worked on styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,12 @@
 import React, { useState } from "react";
-import ReactDom from "react-dom";
 import ExpenseTracker from "./Components/ExpenseTracker";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
 
 function App() {
   useState();
   return (
-    <>
+    <div>
       <ExpenseTracker />
-      <input type="text" /> Name
-      <input type="text" /> Amount
-      <input /> Type
-      <input /> Date
-      {<Button>Add a new expense</Button>}
-      <Container style={{ marginTop: 30 }}>
-        <div class="row">
-          <div class="col"></div>
-          <div class="col"></div>
-        </div>
-      </Container>
-    </>
+    </div>
   );
 }
 
--- my-app/src/Components/ExpenseTracker.js
+++ my-app/src/Components/ExpenseTracker.js
@@ -1,5 +1,32 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
-export default function ExpenseTracker() {
-  return <div>Hello World</div>;
+function ExpenseTracker() {
+  return (
+    <div>
+      <h3 className="navbar">Name</h3>
+      <input type="text" />
+
+      <h3>Amount</h3>
+      <input type="text" />
+
+      <h3>Type</h3>
+      <input />
+
+      <h3>Date</h3>
+      <input />
+
+      {<Button>Add a new expense</Button>}
+
+      <Container style={{ marginTop: 30 }}>
+        <div class="row">
+          <div class="col"></div>
+          <div class="col"></div>
+        </div>
+      </Container>
+    </div>
+  );
 }
+
+export default ExpenseTracker;
