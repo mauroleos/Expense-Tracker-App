6f14c8ffc690effeb4f751f656586af68d21786c
mauro Leos <mauroleos94@gmail.com>
2020-08-06 20:29:54 -0500

    Added table file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import ExpenseTrackerForm from "./Components/ExpenseTracker";
+import ExpenseTrackerTable from "./Components/ExpenseTrackerTable";
 
 function App() {
   useState();
   return (
     <div>
       <ExpenseTrackerForm />
+      <ExpenseTrackerTable />
     </div>
   );
 }
---
+++ my-app/src/Components/ExpenseTrackerTable.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ExpenseTrackerTable extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <table></table>;
+  }
+}
+
+export default ExpenseTrackerTable;
