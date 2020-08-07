503b678d028b6fbffe93313bd17134bd8b5d7272
mauro Leos <mauroleos94@gmail.com>
2020-08-07 01:02:18 -0500

    Finished setting up initial layout

--- my-app/src/Components/ExpenseTracker.js
+++ my-app/src/Components/ExpenseTracker.js
@@ -3,14 +3,35 @@ import React, { Component } from "react";
 class ExpenseTrackerForm extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      type: "",
+      description: "",
+      date: "",
+      amount: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit() {}
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label>
-          Type
-          <select>
+          Type:
+          <select
+            value={this.state.type}
+            onChange={this.handleChange}
+            name="type"
+          >
+            <option value="">-- Please Choose a Type --</option>
             <option value="Card">Card</option>
             <option value="Cash">Cash</option>
             <option value="Cryptocoin">Cryptocoin</option>
@@ -18,7 +39,34 @@ class ExpenseTrackerForm extends Component {
           </select>
         </label>
         <br />
-        <input type="text" placeholder="What did you spend it on"></input>
+        <input
+          type="text"
+          value={this.state.description}
+          name="description"
+          placeholder="Description"
+          onChange={this.handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          value={this.state.date}
+          name="date"
+          placeholder="Date"
+          onChange={this.handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          value={this.state.amount}
+          name="amount"
+          placeholder="Amount"
+          onChange={this.handleChange}
+        />
+        <h1>{this.state.type}</h1>
+        <h1>{this.state.description}</h1>
+        <h1>{this.state.date}</h1>
+        <h1>{this.state.amount}</h1>
+        <button>Submit</button>
       </form>
     );
   }
--- my-app/src/Components/ExpenseTrackerTable.js
+++ my-app/src/Components/ExpenseTrackerTable.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+// import ExpenseTrackerForm from "./Components/ExpenseTracker";
 
 class ExpenseTrackerTable extends Component {
   constructor() {
