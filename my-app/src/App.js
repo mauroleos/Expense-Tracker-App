e51d48144d2e14ae7e745e700b48875af686be8e
mauro Leos <mauroleos94@gmail.com>
2020-08-28 11:41:32 -0500

    added fixed bootstrap and modified files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import Form from "./Components/FormContainer";
 import "./Components/App.css";
-// import "bootstrap";
-// import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   useState();
